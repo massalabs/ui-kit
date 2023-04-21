435745e142d19c334731c9f09dc35d380eabf66a
0xMazout <lf@massa.net>
2023-04-21 17:08:14 +0200

    Update

---
+++ prettierrc.js
@@ -0,0 +1,23 @@
+export default {
+    ...require('@massalabs/prettier-config-as'),
+    overrides: [
+      {
+        files: '.json',
+        options: {
+          parser: 'json',
+        },
+      },
+      {
+        files: '.html',
+        options: {
+          parser: 'html',
+        },
+      },
+      {
+        files: '*.css',
+        options: {
+          parser: 'css',
+        },
+      },
+    ],
+  };
