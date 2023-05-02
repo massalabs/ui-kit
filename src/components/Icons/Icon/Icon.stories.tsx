11ce1c17951c8fbd0b5ae017d0a59f4b43604686
Pivi <pivi@Pivis-MacBook-Air.local>
2023-05-02 16:24:59 +0200

    deleted icon component + reformated tests

--- src/components/Icons/Icon/Icon.stories.tsx
+++ src/components/Icons/Icon/Icon.stories.tsx
@@ -1,33 +1,15 @@
 import { FiSun } from "react-icons/fi";
-import { IconContext } from "react-icons";
-import { Icon } from "./Icon";
 // https://react-icons.github.io/react-icons/icons?name=fi
 // https://github.com/react-icons/react-icons
 
-export default { title: "Icons", component: Icon };
+export default { title: "Icons", component: FiSun };
 
-const args = {
-  src: <FiSun />,
-  color: "#1AE19D",
-  size: "1em",
-};
-
-// to render it as a generic Icon component
-export const _Icon = {
-  render: () => <Icon {...args} />,
-};
-
-// to render it directly without any wrapping component
-export const _Sun = {
+// to render the icon
+// set icon color using className and then normal tailwind presents
+export const _Icons = {
   render: () => (
-    <IconContext.Provider
-      value={{
-        color: "red",
-        className: "",
-        size: "1em",
-      }}
-    >
-      <FiSun onClick={() => console.log("click")} />
-    </IconContext.Provider>
+    <div data-testid="fi-sun">
+      <FiSun onClick={() => console.log("click")} className="text-brand" />
+    </div>
   ),
 };
--- src/components/Icons/Icon/Icon.test.tsx
+++ src/components/Icons/Icon/Icon.test.tsx
@@ -1,41 +1,30 @@
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { IconContext } from "react-icons";
 import { FiSun } from "react-icons/fi";
-// import { Icon } from "./Icon";
 
 describe("Icon", () => {
-  // it("renders the correct icon and calls onClick function when clicked", () => {
-  //   const onClickMock = jest.fn();
-  //   const { getByTestId } = render(
-  //     <Icon
-  //       src={<FiSun />}
-  //       color="#1AE19D"
-  //       size="1em"
-  //       onClick={onClickMock}
-  //     />
-  //   );
-  //   const iconElement = getByTestId("icon");
-  //   expect(iconElement).toBeInTheDocument();
-  //   fireEvent.click(iconElement);
-  //   expect(onClickMock).toHaveBeenCalled();
-  // });
-
-  it("renders the correct icon and calls onClick function when clicked", () => {
-    const onClickMock = jest.fn();
+  it("should render the correct icon", () => {
     const { getByTestId } = render(
-      <IconContext.Provider
-        value={{
-          color: "red",
-          className: "",
-          size: "1em",
-        }}
-      >
-        <FiSun onClick={onClickMock} data-testid="FiSun" />
-      </IconContext.Provider>
+      <div>
+        {" "}
+        <FiSun data-testid="fi-sun" />{" "}
+      </div>
     );
-    const iconElement = getByTestId("FiSun");
+    const iconElement = getByTestId("fi-sun");
     expect(iconElement).toBeInTheDocument();
+  });
+
+  it("shoudl call onClick function when clicked", () => {
+    const onClickMock = jest.fn();
+
+    const { getByTestId } = render(
+      <div>
+        {" "}
+        <FiSun onClick={onClickMock} data-testid="fi-sun" />{" "}
+      </div>
+    );
+    const iconElement = getByTestId("fi-sun");
+
     fireEvent.click(iconElement);
     expect(onClickMock).toHaveBeenCalled();
   });
