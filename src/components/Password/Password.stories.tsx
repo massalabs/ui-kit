1cf3b9bff820d6b57d41014ff00bda47058e4a9d
MaZ <mz@massa.net>
2023-05-02 16:45:45 +0200

    add password component

---
+++ src/components/Password/Password.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Password } from "./Password";
+
+export default { title: "password", component: Password };
+
+export const _Password = {
+  render: () => <Password placeholder={"password"} />,
+};
