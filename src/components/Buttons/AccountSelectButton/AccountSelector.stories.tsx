a011f8bd2bd101ca205d1ad1d746629b87105e5e
Pivi <pivi@Pivis-MacBook-Air.local>
2023-05-02 17:38:44 +0200

    added select account button + default theme variation

--- src/components/Buttons/AccountSelectButton/AccountSelector.stories.tsx
+++ src/components/Buttons/AccountSelectButton/AccountSelector.stories.tsx
@@ -1,4 +1,5 @@
 import { FiUser } from "react-icons/fi";
+<<<<<<< HEAD
 import { FiClock } from "react-icons/fi";
 // import { MassaToken } from "../../../assets/svg-components/MassaToken";
 import { AccountSelectorButton } from "./AccountSelectorButton";
@@ -19,10 +20,25 @@ const args = {
   // You have to pass theme in order to set corrent masstoken bg
   svg: <FiClock className="text-neutral" />,
   accountbalance: "0,000.00",
+=======
+import { MassaToken } from "../../../assets/svg-components/MassaToken";
+import { AccountSelector } from "./AccountSelector";
+
+export default { title: "buttons", component: AccountSelector };
+
+const theme = "theme-light";
+
+const args = {
+  profileImage: <FiUser className="text-neutral" />,
+  accountName: "account #",
+  massaToken: <MassaToken theme={theme} />,
+  accountBalance: "0,000.00",
+>>>>>>> 4352c6d (added select account button + default theme variation)
 };
 
 export const _AccountSelector = {
   render: () => (
+<<<<<<< HEAD
     // this div below is used to simulate the theme
     <div className={theme + " bg-primary w-full h-fit  p-6 flex flex-col"}>
       <p className="text-neutral"> {theme} </p>
@@ -31,6 +47,10 @@ export const _AccountSelector = {
         <p className="text-neutral"> {theme} </p>
         <AccountSelectorButton {...args} />
       </div>
+=======
+    <div className={theme}>
+      <AccountSelector {...args} />
+>>>>>>> 4352c6d (added select account button + default theme variation)
     </div>
   ),
 };
