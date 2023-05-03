225580f0e4d76e53a235b72c89f2cc119de2c6ad
Maxime Carabina <maxime.carabina@epitech.eu>
2023-05-03 10:37:43 +0200

    uncomment line height for button class

--- presets/massa-station-preset.js
+++ presets/massa-station-preset.js
@@ -97,7 +97,7 @@ module.exports = {
           fontSize: "16px",
           fontWeight: "600",
           fontFamily: theme("fontFamily.Urbane"),
-          // lineHeight: "19px",
+          lineHeight: "19px",
         },
         ".mas-menu-active": {
           fontSize: "16px",
