f411a7809de146edcd302d0e761329a440af867f
0xMazout <lf@massa.net>
2023-05-03 15:33:49 +0200

    Add genric color and brand default

--- src/components/LinkIcon/LinkIcon.tsx
+++ src/components/LinkIcon/LinkIcon.tsx
@@ -1,17 +1,23 @@
 import React, { ComponentPropsWithoutRef, useState } from "react";
+import { IconType } from "react-icons/lib";
+import { IconContext } from "react-icons";
 
 export interface LinkIconProps extends ComponentPropsWithoutRef<"button"> {
   children?: React.ReactNode;
-  icon?: string;
+  icon?: IconType;
+  color?: string;
 }
 
 export function LinkIcon(props: LinkIconProps) {
   const [isActive, setisActive] = useState(false);
-  let styleDiv = `flex mas-menu-default  ${isActive ? " bg-primary" : ""} 
-                  rounded-lg p-3 items-center text-neutral hover:bg-tertiary 
-`;
+  let styleDiv = `flex mas-menu-default rounded-lg p-3 items-center text-neutral hover:bg-tertiary`;
+  if (isActive) {
+    styleDiv += ` bg-primary`;
+  }
   const onclickHandler = () => {
-    props.onClick;
+    if (props.onClick) {
+      props.onClick;
+    }
     setisActive((current) => !current);
   };
   return (
@@ -21,7 +27,13 @@ export function LinkIcon(props: LinkIconProps) {
       onClick={onclickHandler}
       {...props}
     >
-      {props.icon && <img src={props.icon} alt="icon" />}
+      <IconContext.Provider
+        value={{
+          color: `hsl(var(--twc-${props.color ? props.color : "brand"}))`,
+        }}
+      >
+        {props.icon && <props.icon className="" />}
+      </IconContext.Provider>
       <p className="ml-2">{props.children}</p>
     </button>
   );
