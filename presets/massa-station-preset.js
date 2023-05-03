63a20ddec613797b6cc8289bd5ae7979b6117155
Pivi <pivi@Pivis-MacBook-Air.local>
2023-05-03 14:52:28 +0200

    Refactored AccSelectBtn + test suits

--- presets/massa-station-preset.js
+++ presets/massa-station-preset.js
@@ -60,6 +60,7 @@ module.exports = {
       dark: {
         primary: colorDarkBlue,
         secondary: colorBasicBlue,
+        secondaryAlt: colorLightGrey,
         tertiary: colorLightBlue,
         brand: colorGreen,
         neutral: colorWhite,
