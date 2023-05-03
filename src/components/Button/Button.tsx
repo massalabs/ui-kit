0027e660bdbe58a3a1758e8fb70d4f4337412598
Maxime Carabina <maxime.carabina@epitech.eu>
2023-05-03 10:14:07 +0200

    completed primary button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,8 +2,8 @@ import React, { ComponentPropsWithoutRef } from "react";
 
 export interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
   children?: React.ReactNode;
-  iconStart?: string;
-  iconEnd?: string;
+  iconStart?: JSX.Element;
+  iconEnd?: JSX.Element;
 }
 
 export function Button(props: ButtonProps) {
@@ -12,23 +12,15 @@ export function Button(props: ButtonProps) {
       data-testid="primary-button"
       type="button"
       className={
-        "px-4 py-3 rounded-lg text-primary bg-default hover:bg-hover " +
-        "active:bg-pressed disabled:bg-disabled_primary disabled:cursor-not-allowed"
+        "px-4 py-3 rounded-lg text-primary bg-default_primary hover:bg-hover_primary " +
+        "active:bg-pressed_primary disabled:bg-disabled_primary disabled:cursor-not-allowed disabled:text-neutral"
       }
       {...props}
     >
       <div className="flex mas-buttons gap-[8px]">
-        {props.iconStart && (
-          <span>
-            <img src={props.iconStart} alt="icon-start" />
-          </span>
-        )}
+        <div className="m-auto">{props.iconStart && props.iconStart}</div>
         {props.children}
-        {props.iconEnd && (
-          <span>
-            <img src={props.iconEnd} alt="icon-end" />
-          </span>
-        )}
+        <div className="m-auto">{props.iconEnd && props.iconEnd}</div>
       </div>
     </button>
   );
