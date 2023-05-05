4565ba48930b004e99f884fc3ae8ed7a9dec909d
MaZ <mz@massa.net>
2023-05-05 11:53:06 +0200

    update password to apply global style

--- src/components/Password/Password.stories.tsx
+++ src/components/Password/Password.stories.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { Password } from "./Password";
 
-export default { title: "password", component: Password };
+export default { title: "Components/Password", component: Password };
 
 export const _Password = {
   render: () => <Password placeholder={"password"} />,
