e8870bb0354c0b17f290a63945004d4acf7986ab
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2023-05-05 18:05:13 +0200

    Added SvgIcon component (#41)
    * Added SvgIcon component
    
    * add svg props
    
    * optimized svg component import
    
    * Clean up files
    
    ---------
    
    Co-authored-by: Pivi <pivi@pivis-air.home>

---
+++ src/components/Icons/Svg/SvgIcon.stories.tsx
@@ -0,0 +1,12 @@
+import { MassaToken } from "../../../assets/svg-components/MassaToken";
+import { MassaLogo } from "../../../assets/svg-components/MassaLogo";
+
+export default { title: "Icons/Svg", component: MassaLogo };
+
+export const _MassaToken = {
+  render: () => <MassaToken size={40} />,
+};
+
+export const _MassaLogo = {
+  render: () => <MassaLogo size={100} />,
+};
