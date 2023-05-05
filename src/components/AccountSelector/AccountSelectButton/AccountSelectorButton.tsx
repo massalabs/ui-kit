bd6c5976e54f1b183e5087a77f985dded6dad136
Pivi <pivi@pivis-air.home>
2023-05-05 19:55:43 +0200

    add selector

---
+++ src/components/AccountSelector/AccountSelectButton/AccountSelectorButton.tsx
@@ -0,0 +1,51 @@
+import { ComponentPropsWithoutRef } from "react";
+
+export interface AccountSelectorProps extends ComponentPropsWithoutRef<"div"> {
+  avatar: React.ReactNode;
+  accountName: string;
+  icon: React.ReactNode;
+  amount: string;
+  children?: React.ReactNode;
+}
+
+export function AccountSelectorButton(props: AccountSelectorProps) {
+  let { avatar, accountName, icon, amount, children, ...rest } = props;
+
+  const accountSelectorClass = "flex flex-row items-center justify-between";
+
+  const hoverClass = `hover:bg-info`;
+  const activeClass = `active:bg-c-pressed`;
+
+  const containerFlexClass = "flex justify-center items-center";
+
+  return (
+    <div
+      className={`bg-neutral text-primary h-14 w-full p-3 rounded-lg mas-menu-active cursor-pointer 
+                      ${accountSelectorClass} 
+                      ${hoverClass} 
+                      ${activeClass}`}
+      {...props}
+      data-testid="account-selector-button"
+    >
+      <div className={`${containerFlexClass}`}>
+        <div
+          className={`bg-primary h-8 w-8 rounded-full mr-2
+                           ${containerFlexClass}`}
+        >
+          {avatar}
+        </div>
+        <div>{accountName}</div>
+      </div>
+
+      <div className={`${containerFlexClass}`}>
+        <div
+          className={`mr-2 bg-primary h-6 w-6 rounded-full
+                          ${containerFlexClass}`}
+        >
+          {icon}
+        </div>
+        <div>{amount}</div>
+      </div>
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export * from "./AccountSelector";
 export * from "./Password";
 export * from "./Input";
 export * from "./Button";
