ba9163557c72b0bb3bb29995251ecbd278590099
Maxime Carabina <maxime.carabina@epitech.eu>
2023-05-05 15:58:10 +0200

    add primary button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, ComponentPropsWithoutRef } from "react";
+
+export interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
+  children?: ReactNode;
+  preIcon?: ReactNode;
+  posIcon?: ReactNode;
+  variant?: "secondary" | undefined;
+}
+
+export function Button(props: ButtonProps) {
+  const { children, preIcon, posIcon, variant, ...rest } = props;
+
+  const variantClass = variant
+    ? "text-f-primary " +
+      "hover:bg-neutral/[.15] " +
+      "active:bg-neutral/[.3] " +
+      "disabled:text-f-disabled-1 disabled:bg-c-disabled-2 disabled:border-c-disabled-1"
+    : "bg-c-default text-primary " +
+      "hover:bg-c-hover " +
+      "active:bg-c-pressed " +
+      "disabled:text-f-disabled-2 disabled:bg-c-disabled-1 disabled:border-c-disabled-1";
+
+  return (
+    <button
+      data-testid="button"
+      type="button"
+      className={`default-button w-full px-4 py-3
+        ${variantClass}
+      `}
+      {...rest}
+    >
+      <div className="w-fit m-auto flex gap-2">
+        <div className="m-auto">{preIcon}</div>
+        {children}
+        <div className="m-auto">{posIcon}</div>
+      </div>
+    </button>
+  );
+}
