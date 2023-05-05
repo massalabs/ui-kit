76351db91d09f14b7e2f25fa71c1607217b5c78c
Pierre <pierregue75@gmail.com>
2023-05-05 15:40:28 +0200

    updated color in storybook

--- src/components/Colors/Color/Color.tsx
+++ src/components/Colors/Color/Color.tsx
@@ -1,13 +1,42 @@
 import React, { ComponentPropsWithoutRef } from "react";
 
 export interface ColorRectangleProps extends ComponentPropsWithoutRef<"div"> {
-  color: string;
+  theme: string;
 }
 
-export function Color(props: ColorRectangleProps) {
+export function Theme(props: ColorRectangleProps) {
+  // console.log(document.querySelector(props.color)?.classList);
   return (
-    <div className="flex ">
-      <div className={`w-40  h-10 mb-2 ${props.color}`}></div>
+    <div className={`mb-2 ${props.theme}`}>
+      <p className="w-40 h-10 text-center">{props.theme}</p>
+      <Color color="bg-primary" />
+      <Color color="bg-secondary" />
+      <Color color="bg-tertiary" />
+      <Color color="bg-brand" />
+      <Color color="bg-neutral" />
+      <Color color="bg-info" />
+      <Color color="bg-success" />
+      <Color color="bg-warning" />
+      <Color color="bg-error" />
+    </div>
+  );
+}
+function Color(props: { color: string }) {
+  return <div id={props.color} className={`w-40 h-10 mb-2 ${props.color}`} />;
+}
+export function Alias() {
+  return (
+    <div className="mb-2">
+      <p className="w-40 h-10 mb-2 text-center "></p>
+      <p className="w-40 h-10 mb-2 text-center ">primary</p>
+      <p className="w-40 h-10 mb-2 text-center ">secondary</p>
+      <p className="w-40 h-10 mb-2 text-center ">tertiary</p>
+      <p className="w-40 h-10 mb-2 text-center ">brand</p>
+      <p className="w-40 h-10 mb-2 text-center ">neutral</p>
+      <p className="w-40 h-10 mb-2 text-center ">info</p>
+      <p className="w-40 h-10 mb-2 text-center ">success</p>
+      <p className="w-40 h-10 mb-2 text-center ">warning</p>
+      <p className="w-40 h-10 mb-2 text-center ">error</p>
     </div>
   );
 }
--- src/components/Colors/Colors.tsx
+++ src/components/Colors/Colors.tsx
@@ -1,60 +1,10 @@
-import { Color } from "./Color";
+import { Theme, Alias } from "./Color";
 export function Colors() {
-  const colorBrand = "bg-[#1AE19D]";
-  const colorWhite = "bg-[#FFFFFF]";
-  const colorDarkGrey = "bg-[#DADADA]";
-  const colorLightGrey = "bg-[#F1F1F1]";
-  const colorDarkBlue = "bg-[#151A26]";
-  const colorBasicBlue = "bg-[#1A202E]";
-  const colorLightBlue = "bg-[#2E374C]";
-  const colorWarning = "bg-[#FFA41D]";
-  const colorError = "bg-[#FF4F4F]";
-  const light = [
-    { color: colorWhite, theme: "primary" },
-    { color: colorLightGrey, theme: "secondary" },
-    { color: colorDarkGrey, theme: "tertiary" },
-    { color: colorBrand, theme: "brand" },
-    { color: colorDarkBlue, theme: "neutral" },
-    { color: colorLightBlue, theme: "info" },
-    { color: colorBrand, theme: "success" },
-    { color: colorWarning, theme: "warning" },
-    { color: colorError, theme: "error" },
-  ];
-
-  const dark = [
-    { color: colorDarkBlue, theme: "primary" },
-    { color: colorBasicBlue, theme: "secondary" },
-    { color: colorLightBlue, theme: "tertiary" },
-    { color: colorBrand, theme: "brand" },
-    { color: colorWhite, theme: "neutral" },
-    { color: colorDarkGrey, theme: "info" },
-    { color: colorBrand, theme: "success" },
-    { color: colorWarning, theme: "warning" },
-    { color: colorError, theme: "error" },
-  ];
-
   return (
-    <div className="flex gap-3">
-      <div>
-        {dark.map((item, index) => (
-          <p key={`dark-${index}`} className="w-40 h-10 mb-2">
-            {item.theme}
-          </p>
-        ))}
-      </div>
-      <div className="dark">
-        {dark.map((item, index) => (
-          <Color key={`dark-${index}`} color={item.color} />
-        ))}
-        Dark theme
-      </div>
-
-      <div className="light">
-        {light.map((item, index) => (
-          <Color key={`light-${index}`} color={item.color} />
-        ))}
-        White theme
-      </div>
+    <div className="flex gap-4">
+      <Alias />
+      <Theme theme="theme-light" />
+      <Theme theme="theme-dark" />
     </div>
   );
 }
