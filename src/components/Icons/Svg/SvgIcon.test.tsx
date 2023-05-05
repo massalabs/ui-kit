5f257fbfb9e381f8d366a1c0ddb7dc46f13b13d9
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2023-05-05 18:05:13 +0200

    Added SvgIcon component (#41)
    * Added SvgIcon component
    
    * add svg props
    
    * optimized svg component import
    
    * Clean up files
    
    ---------
    
    Co-authored-by: Pivi <pivi@pivis-air.home>

---
+++ src/components/Icons/Svg/SvgIcon.test.tsx
@@ -0,0 +1,27 @@
+import "@testing-library/jest-dom";
+import { render, fireEvent, screen } from "@testing-library/react";
+import { MassaLogo } from "../../../assets/svg-components/MassaLogo";
+
+describe("Components | Icons | SvgIcon", () => {
+  it("renders with the correct background color", () => {
+    render(<MassaLogo size={40} />);
+
+    let icon = screen.getByTestId("svg-icon");
+
+    expect(icon).toBeTruthy();
+  });
+
+  test("it should trigger onClick event", () => {
+    const onClickMock = jest.fn();
+    const args = {
+      size: 40,
+      onClick: onClickMock(),
+    };
+
+    render(<MassaLogo {...args} />);
+
+    let icon = screen.getByTestId("svg-icon");
+    fireEvent.click(icon);
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
