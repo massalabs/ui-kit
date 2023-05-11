d336a0acd30db103d1a68f4069ea457b761b4608
0xMazout <lf@massa.net>
2023-05-11 17:40:27 +0200

    Export

--- src/hooks/useHover.tsx
+++ src/hooks/useHover.tsx
@@ -1,6 +1,6 @@
 import { MutableRefObject, useState, useRef, useEffect } from "react";
 
-function useHover<T>(): [MutableRefObject<T>, boolean] {
+export function useHover<T>(): [MutableRefObject<T>, boolean] {
   const [value, setValue] = useState<boolean>(false);
   const ref: any = useRef<T | null>(null);
   const handleMouseOver = (): void => setValue(true);
