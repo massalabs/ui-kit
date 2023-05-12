b468941b5ac1fac0e3053a5baf4c38bd433e7842
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-05-12 09:26:51 +0200

    add stepper component (#69)

---
+++ src/components/Stepper/Stepper.stories.tsx
@@ -0,0 +1,21 @@
+import { Stepper } from "./Stepper";
+
+export default { title: "Components/Stepper", component: Stepper };
+
+export const _Stepper = {
+  render: () => (
+    <>
+      Step 1/3
+      <Stepper step={0} steps={["one", "two", "three"]} />
+      <br />
+      Step 2/3
+      <Stepper step={1} steps={["one", "two", "three"]} />
+      <br />
+      Step 3/3
+      <Stepper step={2} steps={["one", "two", "three"]} />
+      <br />
+      Step 3/3 (with check completed)
+      <Stepper step={3} steps={["one", "two", "three"]} />
+    </>
+  ),
+};
