4ce8c8ea6cdf7a569352342213090aff6e26b4fe
Pivi <pivi@pivis-air.home>
2023-05-12 14:56:09 +0200

    add opti tabs

---
+++ src/components/Tabs/Tabs.stories.tsx
@@ -0,0 +1,28 @@
+import { Tabs } from "./Tabs";
+import { Button } from "../Button";
+
+export default {
+  title: "Components/Tabs",
+  component: Tabs,
+};
+
+const tabsConfig = [
+  {
+    label: "Tab 1",
+    content: <Button> Tab 1 component </Button>,
+    onClickTab: () => console.log("Hello"),
+  },
+  {
+    label: "Tab 2",
+    content: <Button> Tab 2 component </Button>,
+  },
+];
+
+const args = {
+  tabsConfig,
+  defaultIndex: 1,
+};
+
+export const _Tabs = {
+  render: () => <Tabs {...args} />,
+};
