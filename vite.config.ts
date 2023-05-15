231f96db8f0a302f85f83ea6d1dd7f7646b0424a
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-05-15 10:56:55 +0200

    add svgr (#75)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      // Set it to `true` to export React component as default.
+      // Notice that it will override the default behavior of Vite.
+      // This avoid to use repeated:
+      //      import { ReactComponent as Logo } from "./logo.svg"; instead
+      //      import Logo from "./logo.svg";
+      exportAsDefault: true,
+    }),
+  ],
 });
