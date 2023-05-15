d4f23ce7522a95cd8cd1a6aa2b9303fff63de2b0
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2023-05-15 11:44:18 +0200

    add std size for icons (#74)

--- src/components/Icons/Svg/SvgComponent/MassaLogo.tsx
+++ src/components/Icons/Svg/SvgComponent/MassaLogo.tsx
@@ -2,8 +2,8 @@ export function MassaLogo({ ...props }) {
   return (
     <div className={`bg-primary w-fit rounded-full`} data-testid="svg-icon">
       <svg
-        width={props.size}
-        height={props.size}
+        width={props?.size ?? 32}
+        height={props?.size ?? 32}
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
