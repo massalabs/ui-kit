c636798c2d75fd76b10da6f81e58dbe41f45adab
0xMazout <lf@massa.net>
2023-05-15 11:25:24 +0200

    Clean MenuLink

--- src/components/Buttons/LinkIcon/MenuLink.tsx
+++ src/components/Buttons/LinkIcon/MenuLink.tsx
@@ -1,33 +1,38 @@
-import React, { ComponentPropsWithoutRef, useState } from "react";
+import {
+  ComponentPropsWithoutRef,
+  useState,
+  ReactNode,
+  MouseEvent,
+} from "react";
 
-export interface MenuLinkProps extends ComponentPropsWithoutRef<"button"> {
-  children?: React.ReactNode;
+export interface MenuLinkProps extends ComponentPropsWithoutRef<"div"> {
+  children?: ReactNode;
   icon?: JSX.Element;
   color?: string;
-  onClick?: () => void; // Add onClick prop of type function that doesn't return anything
 }
 
 export function MenuLink(props: MenuLinkProps) {
   const [isActive, setIsActive] = useState(false);
-  let divStyle = `flex mas-menu-default cursor-pointer rounded-lg p-3 
-  items-center text-neutral hover:bg-tertiary`;
+  const { onClick } = props;
+  const divStyle = `flex mas-menu-default cursor-pointer rounded-lg p-3 
+  items-center text-neutral hover:bg-tertiary `;
+  const divActiveStyle = isActive
+    ? " bg-primary text-neutral"
+    : " bg-neutral text-primary";
 
-  let divActiveStyle = isActive ? "bg-primary" : "bg-brand";
-
-  function onClickHandler(): void {
-    setIsActive(true);
-    if (props.onClick) {
-      // Call the onClick prop function if it exists
-      props.onClick();
-    }
+  function onClickHandler(
+    e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>
+  ): void {
+    setIsActive(!isActive);
+    onClick?.(e);
   }
 
   return (
     <div
       data-testid="link-icon"
       className={`${divStyle}
-                  ${divActiveStyle}}`}
-      onClick={onClickHandler}
+                  ${divActiveStyle}`}
+      onClick={(e) => onClickHandler(e)}
     >
       {props.icon}
       <p className="ml-1">{props.children}</p>
