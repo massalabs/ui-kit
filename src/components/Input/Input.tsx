835988491eef5561580f80e00801f7863b3d19cc
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-05-16 15:29:35 +0200

    remove onChange from input (#84)

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { useState, ComponentPropsWithoutRef, ChangeEvent } from "react";
+import { ComponentPropsWithoutRef } from "react";
 
 export interface InputProps extends ComponentPropsWithoutRef<"input"> {
   placeholder?: string | undefined;
@@ -8,15 +8,10 @@ export interface InputProps extends ComponentPropsWithoutRef<"input"> {
 
 export function Input(props: InputProps) {
   const { error, warning, ...rest } = props;
-  const [field, setField] = useState("");
 
   const errorClass = error ? "border-s-error" : "";
   const warningClass = warning ? "border-s-warning" : "";
   const messageClass = errorClass || warningClass;
-
-  function handleOnChange(e: ChangeEvent<HTMLInputElement>) {
-    setField(e.target.value);
-  }
 
   return (
     <div className="flex-row">
@@ -26,8 +21,6 @@ export function Input(props: InputProps) {
             data-testid="input-field"
             className={`w-full default-input mb-1 ${messageClass}`}
             type="text"
-            value={field}
-            onChange={handleOnChange}
             {...rest}
           />
           <InputMessage error={error} warning={warning} />
