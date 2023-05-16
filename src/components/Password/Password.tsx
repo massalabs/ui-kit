9f68aeab42f668c0daf1a6b0b9907ce9e773c3a5
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-05-16 15:29:27 +0200

    remove onChange from password (#85)

--- src/components/Password/Password.tsx
+++ src/components/Password/Password.tsx
@@ -1,4 +1,4 @@
-import { useState, ComponentPropsWithoutRef, ChangeEvent } from "react";
+import { useState, ComponentPropsWithoutRef } from "react";
 import { FiEyeOff, FiEye } from "react-icons/fi";
 
 export interface PasswordProps extends ComponentPropsWithoutRef<"input"> {
@@ -6,7 +6,7 @@ export interface PasswordProps extends ComponentPropsWithoutRef<"input"> {
 }
 
 export function Password(props: PasswordProps) {
-  const { placeholder } = props;
+  const { placeholder, ...rest } = props;
 
   let iconClass = `w-5 h-5 inline-block align-text-bottom text-neutral`;
 
@@ -29,11 +29,6 @@ export function Password(props: PasswordProps) {
   };
 
   const [{ type, icon }, setType] = useState(close);
-  const [password, setPassword] = useState("");
-
-  function handleOnChange(e: ChangeEvent<HTMLInputElement>) {
-    setPassword(e.target.value);
-  }
 
   function toggleIcon() {
     return type === "password" ? setType(open) : setType(close);
@@ -47,9 +42,8 @@ export function Password(props: PasswordProps) {
             data-testid="password-input"
             className={`w-full default-input`}
             type={type}
-            value={password}
-            onChange={handleOnChange}
             placeholder={placeholder}
+            {...rest}
           />
         </div>
         <div className="inline -ml-9">
