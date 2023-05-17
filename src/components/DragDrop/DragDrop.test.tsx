5c39bd2ee085b37680d98aae211afe099453fa48
MaZ <mz@massa.net>
2023-05-17 18:32:21 +0200

    add drag & drop

---
+++ src/components/DragDrop/DragDrop.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { DragDrop } from "./DragDrop";
+
+describe("Components | DragDrop", () => {
+  test("it should render", () => {
+    render(<DragDrop placeholder="something here to user read" />);
+
+    let dragDrop = screen.getByTestId("drag-drop");
+
+    expect(dragDrop).toBeInTheDocument();
+  });
+});
