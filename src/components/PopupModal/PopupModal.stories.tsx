f9c28c2adb2740aa8803c3b0c4982b6d86fbf43f
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-05-17 12:42:40 +0200

    add popup-modal (#89)

---
+++ src/components/PopupModal/PopupModal.stories.tsx
@@ -0,0 +1,83 @@
+import {
+  PopupModal,
+  PopupModalContent,
+  PopupModalHeader,
+  PopupModalFooter,
+} from "./PopupModal";
+
+export default { title: "Components/PopupModal" };
+
+const args = {
+  onOpen: () => {
+    console.log("event on open");
+  },
+  onClose: () => {
+    console.log("event on close");
+  },
+};
+
+export const _Regular = {
+  render: () => (
+    <div className="h-[450px]">
+      <PopupModal onOpen={args.onOpen} onClose={args.onClose}>
+        <PopupModalHeader>
+          <label className="text-f-primary">The title</label>
+        </PopupModalHeader>
+        <PopupModalContent>
+          <label className="text-f-primary">any content</label>
+        </PopupModalContent>
+        <PopupModalFooter>
+          <label className="text-f-primary">that's footer</label>
+        </PopupModalFooter>
+      </PopupModal>
+    </div>
+  ),
+};
+
+export const _NoHeader = {
+  render: () => (
+    <div className="h-[450px]">
+      <PopupModal onOpen={args.onOpen}>
+        <PopupModalContent>
+          <label className="text-f-primary">any content</label>
+        </PopupModalContent>
+        <PopupModalFooter>
+          <label className="text-f-primary">that's footer</label>
+        </PopupModalFooter>
+      </PopupModal>
+    </div>
+  ),
+};
+
+export const _NoFooter = {
+  render: () => (
+    <div className="h-[450px]">
+      <PopupModal onOpen={args.onOpen} onClose={args.onClose}>
+        <PopupModalHeader>
+          <label className="text-f-primary">The title</label>
+        </PopupModalHeader>
+        <PopupModalContent>
+          <label className="text-f-primary">any content</label>
+        </PopupModalContent>
+      </PopupModal>
+    </div>
+  ),
+};
+
+export const _FullMode = {
+  render: () => (
+    <div className="h-[450px]">
+      <PopupModal fullMode={true}>
+        <PopupModalHeader>
+          <label className="text-f-primary">The title</label>
+        </PopupModalHeader>
+        <PopupModalContent>
+          <label className="text-f-primary">any content</label>
+        </PopupModalContent>
+        <PopupModalFooter>
+          <label className="text-f-primary">that's footer</label>
+        </PopupModalFooter>
+      </PopupModal>
+    </div>
+  ),
+};
---
+++ src/components/PopupModal/PopupModal.test.tsx
@@ -0,0 +1,106 @@
+import { render, screen, within, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import {
+  PopupModal,
+  PopupModalHeader,
+  PopupModalContent,
+  PopupModalFooter,
+} from "./PopupModal";
+
+describe("Components | PopupModal", () => {
+  test("it should render", () => {
+    render(
+      <PopupModal>
+        <PopupModalHeader>The Title</PopupModalHeader>
+        <PopupModalContent>
+          <label className="text-f-primary">any content</label>
+        </PopupModalContent>
+        <PopupModalFooter>
+          <label className="text-f-primary">that's footer</label>
+        </PopupModalFooter>
+      </PopupModal>
+    );
+
+    let popupModal = screen.getByTestId("popup-modal");
+    let popupModalHeader = screen.getByTestId("popup-modal-header");
+    let popupModalContent = screen.getByTestId("popup-modal-content");
+    let popupModalFooter = screen.getByTestId("popup-modal-footer");
+
+    expect(popupModal).toBeInTheDocument();
+    expect(popupModalHeader).toBeInTheDocument();
+    expect(popupModalContent).toBeInTheDocument();
+    expect(popupModalFooter).toBeInTheDocument();
+  });
+
+  test("it should render without header", () => {
+    render(
+      <PopupModal>
+        <PopupModalContent>
+          <label className="text-f-primary">any content</label>
+        </PopupModalContent>
+        <PopupModalFooter>
+          <label className="text-f-primary">that's footer</label>
+        </PopupModalFooter>
+      </PopupModal>
+    );
+
+    let popupModal = screen.getByTestId("popup-modal");
+    let popupModalHeader =
+      within(popupModal).queryByTestId(/popup-modal-header/);
+
+    expect(popupModal).toBeInTheDocument();
+    expect(popupModalHeader).toBeNull();
+  });
+
+  test("it should render without footer", () => {
+    render(
+      <PopupModal>
+        <PopupModalContent>
+          <label className="text-f-primary">any content</label>
+        </PopupModalContent>
+      </PopupModal>
+    );
+
+    let popupModal = screen.getByTestId("popup-modal");
+    let popupModalFooter =
+      within(popupModal).queryByTestId(/popup-modal-footer/);
+
+    expect(popupModal).toBeInTheDocument();
+    expect(popupModalFooter).toBeNull();
+  });
+
+  test("it should fire onOpen event", () => {
+    const onOpenEvent = jest.fn();
+
+    render(
+      <PopupModal onOpen={onOpenEvent}>
+        <PopupModalContent>
+          <label className="text-f-primary">any content</label>
+        </PopupModalContent>
+      </PopupModal>
+    );
+
+    let popupModal = screen.getByTestId("popup-modal");
+
+    fireEvent.click(popupModal);
+    expect(onOpenEvent).toHaveBeenCalled();
+  });
+
+  test("it should fire onClose event", () => {
+    const onCloseEvent = jest.fn();
+
+    render(
+      <PopupModal onClose={onCloseEvent}>
+        <PopupModalHeader>The Title</PopupModalHeader>
+        <PopupModalContent>
+          <label className="text-f-primary">any content</label>
+        </PopupModalContent>
+      </PopupModal>
+    );
+
+    let popupModalCloseButton = screen.getByTestId("popup-modal-header-close");
+
+    fireEvent.click(popupModalCloseButton);
+    expect(onCloseEvent).toHaveBeenCalled();
+  });
+});
