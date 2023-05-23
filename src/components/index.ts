e04eefbcf4bebf2f2458f96159b90815f7320ffd
0xMazout <lf@massa.net>
2023-05-23 11:46:53 +0200

    updt index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ export * from "./Tabs";
 export * from "./Input";
 export * from "./Password";
 export * from "./Button";
-export * from "./Buttons/LinkIcon";
+export * from "./Buttons/MenuLink";
 export * from "./PopupModal";
 export * from "./Stepper";
 export * from "./Dropdown";
