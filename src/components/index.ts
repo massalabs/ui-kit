bbcf0839afad8a8836a94dae32cf974e5ea44e17
MaZ <mz@massa.net>
2023-05-24 17:52:22 +0200

    remove link-menu

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ export * from "./Tabs";
 export * from "./Input";
 export * from "./Password";
 export * from "./Button";
-export * from "./Buttons/LinkIcon";
 export * from "./PopupModal";
 export * from "./Stepper";
 export * from "./Dropdown";
