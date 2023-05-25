19fbd739e2e70cda28c1c36c200c135425c0b706
Pierre GUÉVENEUX <83134981+Pierregvx@users.noreply.github.com>
2023-05-25 10:46:21 +0200

    add missing aliases (#104)

--- src/components/Colors/Color.tsx
+++ src/components/Colors/Color.tsx
@@ -7,41 +7,147 @@ import { ComponentPropsWithoutRef } from "react";
 export interface ColorRectangleProps extends ComponentPropsWithoutRef<"div"> {
   theme: string;
 }
+const baseClass = "w-40 h-10 mb-2";
+function Color({ color }: { color: string }) {
+  return <div id={color} className={`${baseClass} ${color}`} />;
+}
 
-export function Theme(props: ColorRectangleProps) {
+export function Alias() {
+  const classAlias = `${baseClass} text-center`;
+
+  const Colors = () => (
+    <div>
+      <p className={classAlias}>primary</p>
+      <p className={classAlias}>secondary</p>
+      <p className={classAlias}>tertiary</p>
+      <p className={classAlias}>brand</p>
+      <p className={classAlias}>neutral</p>
+      <p className={classAlias}>info</p>
+    </div>
+  );
+
+  const States = () => (
+    <div>
+      <p className={classAlias}>s-success</p>
+      <p className={classAlias}>s-warning</p>
+      <p className={classAlias}>s-error</p>
+      <p className={classAlias}>s-info</p>
+    </div>
+  );
+  const Components = () => (
+    <div>
+      <p className={classAlias}>c-default</p>
+      <p className={classAlias}>c-hover</p>
+      <p className={classAlias}>c-pressed</p>
+      <p className={classAlias}>c-disabled-1</p>
+      <p className={classAlias}>c-disabled-2</p>
+    </div>
+  );
+  const Icons = () => (
+    <div>
+      <p className={classAlias}>i-primary</p>
+      <p className={classAlias}>i-secondary</p>
+      <p className={classAlias}>i-tertiary</p>
+    </div>
+  );
+  const Fonts = () => (
+    <div>
+      <p className={classAlias}>f-primary</p>
+      <p className={classAlias}>f-secondary</p>
+      <p className={classAlias}>f-tertiary</p>
+      <p className={classAlias}>f-disabled-1</p>
+      <p className={classAlias}>f-disabled-2</p>
+    </div>
+  );
+
   return (
-    <div className={`mb-2 ${props.theme}`}>
-      <p className="w-40 h-10 text-center">{props.theme}</p>
+    <div className="mb-2">
+      <br className={baseClass}></br>
+      <p className={baseClass}>Colors : </p>
+      <Colors />
+
+      <p className={baseClass}>States :</p>
+      <States />
+
+      <p className={baseClass}>Components :</p>
+      <Components />
+      <p className={baseClass}>Icons :</p>
+      <Icons />
+
+      <p className={baseClass}>Fonts</p>
+      <Fonts />
+    </div>
+  );
+}
+export function Theme() {
+  const Colors = () => (
+    <div>
       <Color color="bg-primary" />
       <Color color="bg-secondary" />
       <Color color="bg-tertiary" />
       <Color color="bg-brand" />
       <Color color="bg-neutral" />
       <Color color="bg-info" />
+    </div>
+  );
+
+  const States = () => (
+    <div>
       <Color color="bg-s-success" />
       <Color color="bg-s-warning" />
       <Color color="bg-s-error" />
+      <Color color="bg-s-info" />
     </div>
   );
-}
+  const Components = () => (
+    <div>
+      <Color color="bg-c-default" />
+      <Color color="bg-c-hover" />
+      <Color color="bg-c-pressed" />
+      <Color color="bg-c-disabled-1" />
+      <Color color="bg-c-disabled-2" />
+    </div>
+  );
+  const Icons = () => (
+    <div>
+      <Color color="bg-i-primary" />
+      <Color color="bg-i-secondary" />
+      <Color color="bg-i-tertiary" />
+    </div>
+  );
+  const Fonts = () => (
+    <div>
+      <Color color="bg-f-primary" />
+      <Color color="bg-f-secondary" />
+      <Color color="bg-f-tertiary" />
+      <Color color="bg-f-disabled-1" />
+      <Color color="bg-f-disabled-2" />
+    </div>
+  );
 
-function Color(props: { color: string }) {
-  return <div id={props.color} className={`w-40 h-10 mb-2 ${props.color}`} />;
-}
-
-export function Alias() {
   return (
     <div className="mb-2">
-      <p className="w-40 h-10 mb-2 text-center "></p>
-      <p className="w-40 h-10 mb-2 text-center ">primary</p>
-      <p className="w-40 h-10 mb-2 text-center ">secondary</p>
-      <p className="w-40 h-10 mb-2 text-center ">tertiary</p>
-      <p className="w-40 h-10 mb-2 text-center ">brand</p>
-      <p className="w-40 h-10 mb-2 text-center ">neutral</p>
-      <p className="w-40 h-10 mb-2 text-center ">info</p>
-      <p className="w-40 h-10 mb-2 text-center ">success</p>
-      <p className="w-40 h-10 mb-2 text-center ">warning</p>
-      <p className="w-40 h-10 mb-2 text-center ">error</p>
+      <div className={baseClass} />
+
+      <br className="w-40 h-10"></br>
+
+      <Colors />
+
+      <div className={baseClass} />
+
+      <States />
+
+      <div className={baseClass} />
+
+      <Components />
+
+      <div className={baseClass} />
+
+      <Icons />
+
+      <div className={baseClass} />
+
+      <Fonts />
     </div>
   );
 }
--- src/components/Colors/Colors.tsx
+++ src/components/Colors/Colors.tsx
@@ -4,8 +4,7 @@ export function Colors() {
   return (
     <div className="flex gap-4">
       <Alias />
-      <Theme theme="theme-light" />
-      <Theme theme="theme-dark" />
+      <Theme />
     </div>
   );
 }
