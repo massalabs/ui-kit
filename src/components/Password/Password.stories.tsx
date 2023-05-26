1658656849095aeea00ae1986d78c96fc4ffebf4
MaZ <mz@massa.net>
2023-05-26 14:31:28 +0200

    add default values in password storybook

--- src/components/Password/Password.stories.tsx
+++ src/components/Password/Password.stories.tsx
@@ -5,13 +5,50 @@ export default { title: "Components/Password", component: Password };
 export const _Password = {
   render: () => (
     <>
-      <Password placeholder={"something"} />
+      <h3>Controlled components</h3>
+      <Password placeholder={"with no content"} value="" />
+      <br />
+      <Password placeholder={"something"} value="with content" />
+      <br />
+      <Password
+        placeholder={"something"}
+        value=""
+        error="that's an error without content"
+      />
+      <br />
+      <Password
+        placeholder={"something"}
+        value="with content"
+        error="that's an error with content"
+      />
+      <br />
+      <Password
+        placeholder={"something"}
+        value=""
+        warning="that's a warning without content"
+      />
+      <br />
+      <Password
+        placeholder={"something"}
+        value="123"
+        warning="that's a warning with content"
+      />
       <br />
 
-      <Password placeholder={"something"} error="that's an error" />
+      <h3>Uncontrolled components</h3>
+      <Password placeholder={"something"} defaultValue="" />
       <br />
-
-      <Password placeholder={"something"} warning="that's a warning" />
+      <Password
+        placeholder={"something"}
+        defaultValue=""
+        error="that's an error with|without content"
+      />
+      <br />
+      <Password
+        placeholder={"something"}
+        defaultValue=""
+        warning="that's a warning with|without content"
+      />
     </>
   ),
 };
