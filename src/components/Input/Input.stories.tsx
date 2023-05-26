1c78e41f940d11d760d9596c30d4e24278ff8e9c
MaZ <mz@massa.net>
2023-05-26 14:18:02 +0200

    add input variants

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -5,13 +5,50 @@ export default { title: "Components/Input", component: Input };
 export const _Input = {
   render: () => (
     <>
-      <Input placeholder={"something"} />
+      <h3>Controlled components</h3>
+      <Input placeholder={"with no content"} value="" />
+      <br />
+      <Input placeholder={"something"} value="with content" />
+      <br />
+      <Input
+        placeholder={"something"}
+        value=""
+        error="that's an error without content"
+      />
+      <br />
+      <Input
+        placeholder={"something"}
+        value="with content"
+        error="that's an error with content"
+      />
+      <br />
+      <Input
+        placeholder={"something"}
+        value=""
+        warning="that's a warning without content"
+      />
+      <br />
+      <Input
+        placeholder={"something"}
+        value="123"
+        warning="that's a warning with content"
+      />
       <br />
 
-      <Input placeholder={"something"} error="that's an error" />
+      <h3>Uncontrolled components</h3>
+      <Input placeholder={"something"} defaultValue="" />
       <br />
-
-      <Input placeholder={"something"} warning="that's a warning" />
+      <Input
+        placeholder={"something"}
+        defaultValue=""
+        error="that's an error with|without content"
+      />
+      <br />
+      <Input
+        placeholder={"something"}
+        defaultValue=""
+        warning="that's a warning with|without content"
+      />
     </>
   ),
 };
