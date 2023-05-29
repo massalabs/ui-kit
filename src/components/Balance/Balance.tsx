684b4c2c8d0a85f13156236b120d4890f542e280
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-05-29 17:24:08 +0200

    add balance component (#134)

---
+++ src/components/Balance/Balance.tsx
@@ -0,0 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import React from 'react';
+
+import { ComponentPropsWithoutRef } from 'react';
+import { MassaLogo } from '../Icons/Svg/Massa/MassaLogo';
+
+export interface BalanceProps extends ComponentPropsWithoutRef<'div'> {
+  size: 'xs' | 'lg';
+  amount: string;
+  equal?: string;
+}
+
+export function Balance({ ...props }) {
+  const { size = 'lg', amount, equal } = props;
+
+  const isLg = size === 'lg';
+  const sizeClass = isLg ? 'mas-banner mb-1' : 'mas-buttons mb-0.5';
+  const iconClass = isLg ? 'mr-2' : 'mr-1';
+  const logoClass = isLg ? 32 : 16;
+
+  return (
+    <div
+      data-testid="balance"
+      className="flex items-center w-fit p-5 bg-primary"
+    >
+      <MassaLogo size={logoClass} className={iconClass} />
+      <label className={`${sizeClass} text-f-primary`}>{amount}</label>
+      {equal ? (
+        <label className="ml-2 mt-3 body2 text-info">≈ {equal}</label>
+      ) : null}
+    </div>
+  );
+}
