2c2e25453c13fb8f050f0f417848233b21504843
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-05-30 14:52:03 +0200

    update input fields API (#137)

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,8 +1,9 @@
 import { Input } from './Input';
 
-export default { title: 'Components/Input', component: Input };
+export default { title: 'Components/Input' };
+import { FiUsers } from 'react-icons/fi';
 
-export const _Input = {
+export const _RegularInput = {
   render: () => (
     <>
       <h3>Controlled components</h3>
@@ -48,6 +49,21 @@ export const _Input = {
         placeholder={'something'}
         defaultValue=""
         warning="that's a warning with|without content"
+      />
+    </>
+  ),
+};
+
+export const _IconInput = {
+  render: () => (
+    <>
+      <Input placeholder={'something'} icon={<FiUsers />} defaultValue="" />
+      <br />
+      <Input
+        placeholder={'something'}
+        defaultValue=""
+        icon={<FiUsers />}
+        onClickIcon={() => console.log('clicked')}
       />
     </>
   ),
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,14 +2,66 @@
 // @ts-ignore
 import React from 'react';
 
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, ChangeEvent, cloneElement } from 'react';
 
 export interface InputProps extends ComponentPropsWithoutRef<'input'> {
   error?: string | undefined;
   warning?: string | undefined;
+  icon?: JSX.Element;
+  onClickIcon?: () => void;
 }
 
 export function Input(props: InputProps) {
+  const { icon } = props;
+
+  return icon ? <IconInput {...props} /> : <RawInput {...props} />;
+}
+
+export function IconInput(props: InputProps) {
+  const { error, warning, icon, onClickIcon, ...rest } = props;
+
+  const errorClass = error ? 'border-s-error' : '';
+  const warningClass = warning ? 'border-s-warning' : '';
+  const messageClass = errorClass || warningClass;
+  const clonedIcon = icon
+    ? cloneElement(icon, {
+        className: 'w-5 h-5 inline-block align-text-bottom text-neutral',
+      })
+    : null;
+
+  function handleOnClick(e: ChangeEvent<unknown>) {
+    e.preventDefault();
+
+    onClickIcon?.();
+  }
+
+  return (
+    <div className="flex-row">
+      <div className="grid-cols-2">
+        <div className="inline h-12">
+          <input
+            data-testid="password-input"
+            className={`w-full default-input mb-1 ${messageClass}`}
+            type="text"
+            {...rest}
+          />
+        </div>
+        <div className="inline -ml-9">
+          <button
+            className="w-8 h-8 bg-transparent"
+            data-testid="password-icon"
+            onClick={handleOnClick}
+          >
+            {clonedIcon}
+          </button>
+        </div>
+        <InputMessage error={error} warning={warning} />
+      </div>
+    </div>
+  );
+}
+
+export function RawInput(props: InputProps) {
   const { error, warning, ...rest } = props;
 
   const errorClass = error ? 'border-s-error' : '';
