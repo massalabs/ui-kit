2a256ede7f82ff09598cd7bd9789faf327117552
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-05-31 19:07:19 +0200

    update account update API (#150)

--- src/components/AccountSelector/AccountSelector.stories.tsx
+++ src/components/AccountSelector/AccountSelector.stories.tsx
@@ -1,19 +1,24 @@
-import { FiUser } from 'react-icons/fi';
+import { FiArrowUpRight } from 'react-icons/fi';
 import { MassaToken } from '../Icons/Svg/Massa/MassaToken';
 import { AccountSelector } from './AccountSelector';
 
 export default {
   title: 'Components/Account Selector',
-  component: AccountSelector,
 };
 
 const args = {
-  avatar: <FiUser className="text-primary h-6 w-6" />,
+  avatar: <FiArrowUpRight />,
   accountName: 'account #',
-  icon: <MassaToken size={24} className="bg-neutral rounded-full" />,
+  icon: <MassaToken size={24} />,
   amount: '0,000.00',
 };
 
-export const _AccountSelector = {
-  render: () => <AccountSelector {...args} />,
+export const _Primary = {
+  render: () => <AccountSelector {...args} customClass="w-1/2" />,
+};
+
+export const _Secondary = {
+  render: () => (
+    <AccountSelector variant="secondary" customClass="w-1/2" {...args} />
+  ),
 };
--- src/components/AccountSelector/AccountSelector.tsx
+++ src/components/AccountSelector/AccountSelector.tsx
@@ -2,41 +2,69 @@
 // @ts-ignore
 import React from 'react';
 
-import { ReactNode, ComponentPropsWithoutRef } from 'react';
+import { ReactNode, ComponentPropsWithoutRef, cloneElement } from 'react';
 
 export interface AccountSelectorProps extends ComponentPropsWithoutRef<'div'> {
   avatar: JSX.Element;
   accountName: string;
   icon: JSX.Element;
   amount: string;
+  customClass?: string;
+  variant?: 'secondary' | undefined;
   children?: ReactNode;
 }
 
 export function AccountSelector(props: AccountSelectorProps) {
-  let { avatar, accountName, icon, amount, children, ...rest } = props;
+  let {
+    avatar,
+    accountName,
+    icon,
+    amount,
+    variant,
+    children,
+    customClass,
+    ...rest
+  } = props;
 
-  const containerFlexClass = 'flex justify-center items-center';
+  const containerClass = 'flex justify-center items-center';
+  const primaryClass = `bg-c-default text-primary
+                        hover:bg-c-hover hover:border-c-hover
+                        active:bg-c-hover 
+                        disabled:text-f-disabled-2 disabled:bg-c-disabled-1 disabled:border-c-disabled-1`;
+  const secondaryClass = `text-f-primary
+                          hover:bg-neutral/[.15]
+                          active:bg-neutral/[.3]                                                  
+                          disabled:text-f-disabled-1 disabled:bg-c-disabled-2 disabled:border-c-disabled-1`;
+
+  const primaryAvatarClass = 'bg-primary text-f-primary';
+  const secondaryAvatarClass = 'bg-neutral text-f-secondary';
+
+  const selectorClass = variant ? secondaryClass : primaryClass;
+  const avatarClass = variant ? secondaryAvatarClass : primaryAvatarClass;
+
+  const clonedAvatar = avatar
+    ? cloneElement(avatar, {
+        className: `${avatarClass} rounded-full p-1 h-8 w-8 mr-2`,
+      })
+    : null;
 
   return (
     <div
       data-testid="account-selector-button"
-      className={`bg-primary text-f-primary h-14 w-full p-3 rounded-lg mas-menu-active cursor-pointer 
+      className={`h-14 w-full p-3 rounded-lg mas-menu-active cursor-pointer 
                   flex flex-row items-center justify-between
-                  hover:bg-tertiary
-                  active:bg-secondary`}
+                  ${containerClass}
+                  ${selectorClass}
+                  ${customClass}`}
       {...rest}
     >
-      <div className={`${containerFlexClass}`}>
-        <div
-          className={`bg-neutral h-8 w-8 rounded-full mr-2 ${containerFlexClass}`}
-        >
-          {avatar}
-        </div>
-        <div>{accountName}</div>
+      <div className={`${containerClass}`}>
+        {clonedAvatar}
+        {accountName}
       </div>
-      <div className={`${containerFlexClass}`}>
-        <div className={`mr-2 rounded-full ${containerFlexClass}`}>{icon}</div>
-        <div>{amount}</div>
+      <div className={`${containerClass} gap-2`}>
+        {icon}
+        {amount}
       </div>
     </div>
   );
