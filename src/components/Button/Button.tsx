c97335e385f48158998dd9b19d2ae99f96184318
MaZ <mz@massa.net>
2023-06-01 01:11:40 +0200

    update button styles and api

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,30 +9,30 @@ export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   children?: ReactNode;
   preIcon?: ReactNode;
   posIcon?: ReactNode;
-  variant?: 'secondary' | undefined;
+  variant?: 'secondary' | 'primary' | undefined;
   customClass?: string;
 }
 
 export function Button(props: ButtonProps) {
-  const { children, preIcon, posIcon, variant, customClass, ...rest } = props;
+  const {
+    children,
+    preIcon,
+    posIcon,
+    variant = 'primary',
+    customClass,
+    ...rest
+  } = props;
 
-  const primaryClass = `bg-c-default text-primary
-                        hover:bg-c-hover hover:border-c-hover
-                        active:bg-c-hover 
-                        disabled:text-f-disabled-2 disabled:bg-c-disabled-1 disabled:border-c-disabled-1`;
-
-  const secondaryClass = `text-f-primary
-                          hover:bg-neutral/[.15]
-                          active:bg-neutral/[.3]                                                  
-                          disabled:text-f-disabled-1 disabled:bg-c-disabled-2 disabled:border-c-disabled-1`;
-
-  const buttonClass = variant ? secondaryClass : primaryClass;
+  const classes: any = {
+    primary: 'default-primary',
+    secondary: 'default-secondary',
+  };
 
   return (
     <button
       data-testid="button"
       type="button"
-      className={`default-button px-4 py-3 ${buttonClass} ${customClass}`}
+      className={`default-button px-4 py-3 ${classes[variant]} ${customClass}`}
       {...rest}
     >
       <div className="w-fit m-auto flex gap-2 items-baseline">
