4c0c1a5fa88228b390e4d96bd28acfdd1d5423ad
MaZ <mz@massa.net>
2023-06-01 01:20:35 +0200

    move AccountSelector to Selector

---
+++ src/components/Selector/Selector.tsx
@@ -0,0 +1,73 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import React from 'react';
+
+import { ReactNode, ComponentPropsWithoutRef, cloneElement } from 'react';
+
+export interface SelectorProps extends ComponentPropsWithoutRef<'div'> {
+  preIcon: JSX.Element;
+  content: string;
+  posIcon: JSX.Element;
+  amount: string;
+  customClass?: string;
+  variant?: 'secondary' | undefined;
+  children?: ReactNode;
+}
+
+interface Classes {
+  [key: string]: string | object;
+}
+
+export function Selector(props: SelectorProps) {
+  let {
+    preIcon,
+    content,
+    posIcon,
+    amount,
+    variant = 'primary',
+    children,
+    customClass,
+    ...rest
+  } = props;
+
+  const CLASSES: Classes = {
+    root: 'flex justify-center items-center',
+    primary: {
+      default: 'default-primary',
+      preIcon: 'bg-primary text-f-primary rounded-full p-1 h-8 w-8 mr-2',
+    },
+    secondary: {
+      default: 'default-secondary',
+      preIcon: 'bg-neutral text-f-secondary rounded-full p-1 h-8 w-8 mr-2',
+    },
+  };
+
+  const VARIANT = CLASSES[variant] as Classes;
+
+  const clonedPreIcon = preIcon
+    ? cloneElement(preIcon, {
+        className: VARIANT.preIcon,
+      })
+    : null;
+
+  return (
+    <div
+      data-testid="selector"
+      className={`h-14 w-full p-3 rounded-lg mas-menu-active cursor-pointer 
+                  flex flex-row items-center justify-between
+                  ${CLASSES.root}
+                  ${VARIANT.default}
+                  ${customClass}`}
+      {...rest}
+    >
+      <div className={`${CLASSES.root}`}>
+        {clonedPreIcon}
+        {content}
+      </div>
+      <div className={`${CLASSES.root} gap-2`}>
+        {posIcon}
+        {amount}
+      </div>
+    </div>
+  );
+}
