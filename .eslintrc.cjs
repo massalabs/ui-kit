d58219f1fac48f4e4e8e3c16e19f57620cf617da
MaZ <mz@massa.net>
2023-06-01 14:36:28 +0200

    new lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,15 @@
 module.exports = {
   extends: ['@massalabs', 'plugin:storybook/recommended'],
+  rules: {
+    'no-console': 'error',
+    'no-debugger': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.{js,ts,tsx}', '**/*.stories.{js,ts,tsx}'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
 };
