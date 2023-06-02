1ae016a2cb8755da5590bf11948ad9098c4a8b6b
lilyanB <91454320+lilyanB@users.noreply.github.com>
2023-06-02 10:28:44 +0200

    add RadioButton (#159)

---
+++ src/components/RadioButton/RadioButton.stories.tsx
@@ -0,0 +1,10 @@
+import { RadioButton } from './RadioButton';
+
+export default {
+  title: 'Components/Radio Button',
+  component: RadioButton,
+};
+
+export const _RadioButton = {
+  render: () => <RadioButton />,
+};
---
+++ src/components/RadioButton/RadioButton.tsx
@@ -0,0 +1,32 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import React from 'react';
+import { ComponentPropsWithoutRef } from 'react';
+
+export type RadioButtonProps = ComponentPropsWithoutRef<'div'>;
+
+export function RadioButton(props: RadioButtonProps) {
+  let { ...rest } = props;
+
+  return (
+    <div data-testid="radio-button">
+      <div className="inline-flex items-center">
+        <label className="relative flex cursor-pointer items-center rounded-full p-3">
+          <input
+            type="radio"
+            className={`peer relative h-5 w-5
+                    cursor-pointer appearance-none rounded-full
+                    border-2 border-gray-400 checked:border-green-500`}
+            {...rest}
+          />
+          <div
+            className={`pointer-events-none absolute top-2/4 left-2/4
+                      -translate-y-2/4 -translate-x-2/4 bg-green-500
+                      w-2.5 h-2.5 rounded-full opacity-0
+                      transition-opacity peer-checked:opacity-100`}
+          ></div>
+        </label>
+      </div>
+    </div>
+  );
+}
