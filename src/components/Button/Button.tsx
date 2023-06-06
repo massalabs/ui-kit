2eaaa6cef9fd080213f730a4a76ec38c9a0670d0
MaZ <mz@massa.net>
2023-06-06 14:44:02 +0200

    add button versions

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,8 +9,50 @@ export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   children?: ReactNode;
   preIcon?: ReactNode;
   posIcon?: ReactNode;
-  variant?: 'secondary' | 'primary' | 'danger' | undefined;
+  model?: 'single' | 'border';
+  variant?: 'secondary' | 'primary' | 'danger' | 'toggle' | 'icon' | undefined;
   customClass?: string;
+}
+
+export function ButtonToggle(props: ButtonProps) {
+  const { children, customClass, ...rest } = props;
+
+  return (
+    <button
+      data-testid="button"
+      type="button"
+      className={`default-button default-toggle ${customClass}`}
+      {...rest}
+    >
+      <div className="w-fit m-auto flex gap-2 items-baseline">{children}</div>
+    </button>
+  );
+}
+
+export function ButtonIcon(props: ButtonProps) {
+  const { children, model = 'single', customClass, ...rest } = props;
+
+  const models: Models = {
+    single:
+      'disabled:text-tertiary disabled:bg-secondary disabled:border-tertiary disabled:hover:border-tertiary',
+    border:
+      'border-0 bg-secondary hover:bg-tertiary disabled:bg-secondary disabled:text-tertiary',
+  };
+
+  interface Models {
+    [key: string]: string | object;
+  }
+
+  return (
+    <button
+      data-testid="button"
+      type="button"
+      className={`default-button default-icon ${models[model]} ${customClass}`}
+      {...rest}
+    >
+      <div className="w-fit m-auto flex gap-2 items-baseline">{children}</div>
+    </button>
+  );
 }
 
 export function Button(props: ButtonProps) {
@@ -31,9 +73,18 @@ export function Button(props: ButtonProps) {
     primary: 'default-primary',
     secondary: 'default-secondary',
     danger: 'default-danger',
+    toggle: 'default-toggle',
+    icon: 'default-icon',
   };
 
-  return (
+  const isToggle = variant === 'toggle';
+  const isIcon = variant === 'icon';
+
+  return isToggle ? (
+    <ButtonToggle {...props} />
+  ) : isIcon ? (
+    <ButtonIcon {...props} />
+  ) : (
     <button
       data-testid="button"
       type="button"
