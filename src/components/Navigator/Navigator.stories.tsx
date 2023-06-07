1e0bb147ceadba0ec71914bb5b3504ed85ca3c87
Maxime Carabina <72019005+maxime-carabina@users.noreply.github.com>
2023-06-08 01:24:48 +0200

    refactor Navigator component (#191)

--- src/components/Navigator/Navigator.stories.tsx
+++ src/components/Navigator/Navigator.stories.tsx
@@ -1,39 +1,70 @@
-import { ReactNode } from 'react';
 import { Navigator } from './Navigator';
 
 import { FiHome, FiCodepen, FiGlobe } from 'react-icons/fi';
 
 export default { title: 'Components/Navigator', component: Navigator };
 
-const pages = [
-  {
-    icon: <FiHome />,
-  },
-  {
-    icon: <FiCodepen />,
-  },
-  {
-    icon: <FiGlobe />,
-  },
-];
-
 export const _Navigator = {
   render: () => (
     <>
-      {pages.map((page: { icon: ReactNode }, index: number) => (
-        <>
-          <div key={index}>
-            <Navigator
-              nbPages={pages.length}
-              activePage={{ index: index, icon: page.icon }}
-              onClickNext={() => console.log('Next clicked')}
-              onClickBack={() => console.log('Back clicked')}
-            />
-          </div>
-          <br />
-          <br />
-        </>
-      ))}
+      <Navigator
+        items={[
+          {
+            icon: <FiHome />,
+            isActive: true,
+          },
+          {
+            icon: <FiCodepen />,
+            isActive: false,
+          },
+          {
+            icon: <FiGlobe />,
+            isActive: false,
+          },
+        ]}
+        onClickNext={() => console.log('Next clicked')}
+        onClickBack={() => console.log('Back clicked')}
+      />
+      <br />
+      <br />
+      <Navigator
+        items={[
+          {
+            icon: <FiHome />,
+            isActive: false,
+          },
+          {
+            icon: <FiCodepen />,
+            isActive: true,
+          },
+          {
+            icon: <FiGlobe />,
+            isActive: false,
+          },
+        ]}
+        onClickNext={() => console.log('Next clicked')}
+        onClickBack={() => console.log('Back clicked')}
+      />
+      <br />
+      <br />
+      <Navigator
+        items={[
+          {
+            icon: <FiHome />,
+            isActive: false,
+          },
+          {
+            icon: <FiCodepen />,
+            isActive: false,
+          },
+          {
+            icon: <FiGlobe />,
+            isActive: true,
+          },
+        ]}
+        onClickNext={() => console.log('Next clicked')}
+        onClickBack={() => console.log('Back clicked')}
+      />
     </>
   ),
 };
