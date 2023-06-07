f9e4974cff0b134268cfcb55f908bff0f5d4003e
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2023-06-07 09:51:38 +0200

    We Put Brand Green on Radio Btn (#177)

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -16,12 +16,12 @@ export function RadioButton(props: RadioButtonProps) {
             type="radio"
             className={`peer relative h-5 w-5
                     cursor-pointer appearance-none rounded-full
-                    border-2 border-gray-400 checked:border-green-500`}
+                    border-2 border-gray-400 checked:border-brand`}
             {...rest}
           />
           <div
             className={`pointer-events-none absolute top-2/4 left-2/4
-                      -translate-y-2/4 -translate-x-2/4 bg-green-500
+                      -translate-y-2/4 -translate-x-2/4 bg-brand
                       w-2.5 h-2.5 rounded-full opacity-0
                       transition-opacity peer-checked:opacity-100`}
           ></div>
