cdea7b31868b6740f67830389d363746e6bfa1b5
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-06-14 15:45:58 +0200

    fix currency (#221)

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -6,6 +6,54 @@ import { ComponentPropsWithoutRef } from 'react';
 import { InputMessage } from './../Input/Input';
 import CurrencyInput from 'react-currency-input-field';
 
+function MAS(props: ICurrencyProps) {
+  const { error, warning, variant = 'MAS', ...rest } = props;
+
+  const errorClass = error ? 'border-s-error' : '';
+  const warningClass = warning ? 'border-s-warning' : '';
+  const messageClass = errorClass || warningClass;
+
+  return (
+    <>
+      <CurrencyInput
+        className={`w-full default-input mb-1 ${messageClass}`}
+        data-testid="currency-field"
+        decimalScale={9}
+        decimalsLimit={9}
+        disableAbbreviations={true}
+        allowNegativeValue={false}
+        suffix={' MAS'}
+        {...rest}
+      />
+      <InputMessage error={error} warning={warning} />
+    </>
+  );
+}
+
+function NMAS(props: ICurrencyProps) {
+  const { error, warning, variant = 'MAS', ...rest } = props;
+
+  const errorClass = error ? 'border-s-error' : '';
+  const warningClass = warning ? 'border-s-warning' : '';
+  const messageClass = errorClass || warningClass;
+
+  return (
+    <>
+      <CurrencyInput
+        className={`w-full default-input mb-1 ${messageClass}`}
+        data-testid="currency-field"
+        allowDecimals={false}
+        disableAbbreviations={true}
+        allowNegativeValue={false}
+        suffix={' nMAS'}
+        disableGroupSeparators={true}
+        {...rest}
+      />
+      <InputMessage error={error} warning={warning} />
+    </>
+  );
+}
+
 export interface ICurrencyProps
   extends ComponentPropsWithoutRef<typeof CurrencyInput> {
   error?: string | undefined;
@@ -17,54 +65,6 @@ export function Currency(props: ICurrencyProps) {
   const { variant = 'MAS' } = props;
 
   const isNMas = variant === 'nMAS';
-
-  function MAS(props: ICurrencyProps) {
-    const { error, warning, variant = 'MAS', ...rest } = props;
-
-    const errorClass = error ? 'border-s-error' : '';
-    const warningClass = warning ? 'border-s-warning' : '';
-    const messageClass = errorClass || warningClass;
-
-    return (
-      <>
-        <CurrencyInput
-          className={`w-full default-input mb-1 ${messageClass}`}
-          data-testid="currency-field"
-          decimalScale={9}
-          decimalsLimit={9}
-          disableAbbreviations={true}
-          allowNegativeValue={false}
-          suffix={' MAS'}
-          {...rest}
-        />
-        <InputMessage error={error} warning={warning} />
-      </>
-    );
-  }
-
-  function NMAS(props: ICurrencyProps) {
-    const { error, warning, variant = 'MAS', ...rest } = props;
-
-    const errorClass = error ? 'border-s-error' : '';
-    const warningClass = warning ? 'border-s-warning' : '';
-    const messageClass = errorClass || warningClass;
-
-    return (
-      <>
-        <CurrencyInput
-          className={`w-full default-input mb-1 ${messageClass}`}
-          data-testid="currency-field"
-          allowDecimals={false}
-          disableAbbreviations={true}
-          allowNegativeValue={false}
-          suffix={' nMAS'}
-          disableGroupSeparators={true}
-          {...rest}
-        />
-        <InputMessage error={error} warning={warning} />
-      </>
-    );
-  }
 
   return (
     <div className="flex-row">
