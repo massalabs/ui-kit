48ab97c42d0ad686350eb4355579042a13e4d5c3
Maxime Carabina <72019005+maxime-carabina@users.noreply.github.com>
2023-06-22 15:34:27 +0200

    add variant button for spinner (#259)

--- src/components/Spinner/Spinner.stories.tsx
+++ src/components/Spinner/Spinner.stories.tsx
@@ -1,13 +1,18 @@
 import { Spinner } from './Spinner';
+import { Button } from '../Button/Button';
 
 export default {
   title: 'Components/Spinner',
 };
 
-export const _Spinner = {
+export const _Default = {
+  render: () => <Spinner />,
+};
+
+export const _Button = {
   render: () => (
-    <>
-      <Spinner />
-    </>
+    <Button>
+      <Spinner variant="button" />
+    </Button>
   ),
 };
