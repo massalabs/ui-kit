cf07cbf1508f54c320e1d953540ba332cca33da7
Maxime Carabina <maxime.carabina@epitech.eu>
2023-06-22 13:05:15 +0200

    add variant button for spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,20 +3,27 @@
 import React from 'react';
 
 interface SpinerProps {
-  size?: number;
+  variant?: 'button';
   customClass?: string;
 }
 
 export function Spinner(props: SpinerProps) {
-  const { customClass } = props;
+  const { variant = 'default', customClass } = props;
+
+  const gradientColorClass =
+    variant === 'button'
+      ? 'bg-gradient-to-tr from-neutral via-primary to-primary'
+      : 'bg-gradient-to-tr from-primary via-neutral to-neutral';
+
+  const bgColorClass = variant === 'button' ? 'bg-neutral' : 'bg-primary';
 
   return (
     <div
       data-testid="spinner"
       className={`animate-spin flex items-center justify-center rounded-full w-6 h-6
-      bg-gradient-to-tr from-primary via-neutral to-neutral ${customClass}`}
+      ${gradientColorClass} ${customClass}`}
     >
-      <div className={`w-5 h-5 rounded-full bg-primary`} />
+      <div className={`w-5 h-5 rounded-full ${bgColorClass}`} />
     </div>
   );
 }
