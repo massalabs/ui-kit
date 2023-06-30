48a93a8b68aacb9e6ff9adc64a006f408039fd94
pivi <paul.lartisant@icloud.com>
2023-06-30 17:24:37 +0200

    Add Expandable Address Size

--- src/components/Clipboard/Clipboard.stories.tsx
+++ src/components/Clipboard/Clipboard.stories.tsx
@@ -5,7 +5,7 @@ export default { title: 'Components/Clipboard', component: Clipboard };
 export const _Clipboard = {
   render: () => (
     <Clipboard
-      displayedContent="formatted content"
+      displayedContent="AU121SVmHtJ4tWcFB1eFvcZ7mqp9rDPJ2QkP5TJPeHZE3Kr7uxLV6"
       rawContent="this is the content"
       error="Oupps!"
       success="Copied!"
