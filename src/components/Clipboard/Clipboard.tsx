ed4acfd7c5889783389ecf523a66893e5ab6c667
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2023-09-26 17:41:49 +0200

    Remove clipboard hover (#338)
    Co-authored-by: Pivi <pivi@pivis-air.home>

--- src/components/Clipboard/Clipboard.tsx
+++ src/components/Clipboard/Clipboard.tsx
@@ -32,11 +32,6 @@ export function Clipboard(props: ClipboardProps) {
 
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
-  // check to see if mouse is hovering over the component
-  const [isHovered, setIsHovered] = useState(false);
-  // check params, if undefiend, set to true in handleHover function
-  const [hover, setHover] = useState(toggleHover);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   function handleCopyToClipboard() {
     setError(false);
@@ -56,38 +51,12 @@ export function Clipboard(props: ClipboardProps) {
     }, 3000);
   }
 
-  function displayAddressOnHover() {
-    const divStyle: CSSProperties = {
-      position: 'fixed',
-      left: position.x + 30,
-      top: position.y - 30,
-    };
-
-    return (
-      <p
-        style={divStyle}
-        className="relative bg-secondary z-10 top-2 text-f-primary pr-2 w-fit truncate pointer-events-none px-1 rounded"
-      >
-        {rawContent}
-      </p>
-    );
-  }
-
-  function handleHover(bool: boolean) {
-    if (toggleHover === undefined) setHover(true);
-    setIsHovered(bool);
-  }
-
   return (
     <>
-      {isHovered && hover ? displayAddressOnHover() : null}
       <div
         data-testid="clipboard-field"
         className={`flex flex-row items-center mas-body2 justify-between w-full
         h-12 px-3 rounded bg-secondary cursor-pointer ${customClass}`}
-        onMouseEnter={() => handleHover(true)}
-        onMouseLeave={() => handleHover(false)}
-        onMouseMove={(e) => setPosition({ x: e.clientX, y: e.clientY })}
         onClick={handleCopyToClipboard}
         {...rest}
       >
