cccc734b98fd4a57b573c7a3d701a76815f550f6
MaZ <mz@massa.net>
2023-10-11 15:25:44 +0200

    images conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      // Set it to `true` to export React component as default.
-      // Notice that it will override the default behavior of Vite.
-      // This avoid to use repeated:
-      //      import { ReactComponent as Logo } from "./logo.svg"; instead
-      //      import Logo from "./logo.svg";
-      exportAsDefault: true,
-    }),
-  ],
-});
+export default ({ mode }) => {
+  // loadEnv(mode, process.cwd()) will load the .env files depending on the mode
+  // import.meta.env.VITE_BASE_APP available here with: process.env.VITE_BASE_APP
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [
+      react(),
+      svgr({
+        // Set it to `true` to export React component as default.
+        // Notice that it will override the default behavior of Vite.
+        // This avoid to use repeated:
+        //      import { ReactComponent as Logo } from "./logo.svg"; instead
+        //      import Logo from "./logo.svg";
+        exportAsDefault: true,
+      }),
+    ],
+  });
+};
