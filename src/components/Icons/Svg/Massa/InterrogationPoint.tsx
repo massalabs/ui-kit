cf292888e3a1e8c7e4bb755314e359b7661b588b
Nathan Seva <thykof@protonmail.ch>
2023-12-20 10:09:00 -0500

    change color

--- src/components/Icons/Svg/Massa/InterrogationPoint.tsx
+++ src/components/Icons/Svg/Massa/InterrogationPoint.tsx
@@ -25,7 +25,7 @@ export function InterrogationPoint(props: SVGProps) {
         <path
           d="M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32
           16 32Z"
-          fill={primaryColor ?? '#2E374C'}
+          fill={primaryColor ?? '#1A202E'}
         />
         <path
           d="M14.6641 18.3516L14.4766 15.7422C16.9089 15.7734 18.125 15.1667 18.125 13.9219C18.125 13.3542 17.9036
