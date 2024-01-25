e261d4e10eb01014bc4985442d46ee7604203cde
Nathan Seva <thykof@protonmail.ch>
2024-01-25 10:33:01 +0100

    remove bridge logo

--- src/components/Icons/Svg/Massa/index.ts
+++ src/components/Icons/Svg/Massa/index.ts
@@ -3,6 +3,5 @@ export * from './MassaToken';
 export * from './MassaWallet';
 export * from './StationLogo';
 export * from './Certificate';
-export * from './BridgeLogo';
 export * from './FT1';
 export * from './InterrogationPoint';
