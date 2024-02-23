a17505684b84c1d0c36fb2965f3126a9948101f6
Nathan Seva <thykof@protonmail.ch>
2024-02-23 19:17:29 +0100

    keep one spinner, color props

--- src/components/Spinner/Spinner.stories.tsx
+++ src/components/Spinner/Spinner.stories.tsx
@@ -1,52 +1,15 @@
-import { Spinner, SpinnerSized } from './Spinner';
-import { Button } from '../Button/Button';
+import { Spinner } from './Spinner';
 
 export default {
   title: 'Components/Spinner',
 };
 
 export const _Default = {
-  render: () => <Spinner />,
-};
-
-export const _InAButton = {
-  render: () => (
-    <Button disabled>
-      <Spinner variant="button" />
-    </Button>
-  ),
-};
-
-export const _VariantButton = {
   render: () => (
     <div className="flex space-x-4">
-      <Spinner variant="button" />
-      <Button disabled>
-        <Spinner />
-      </Button>
-    </div>
-  ),
-};
-
-export const _Sized = {
-  render: () => (
-    <div className="flex space-x-4">
-      <SpinnerSized size="sm" />
-      <SpinnerSized size="md" />
-      <SpinnerSized size="lg" />
-    </div>
-  ),
-};
-
-export const _SizedButton = {
-  render: () => (
-    <div className="flex space-x-4">
-      <Button>
-        <SpinnerSized size="sm" />
-      </Button>
-      <Button disabled>
-        <SpinnerSized size="md" />
-      </Button>
+      <Spinner size="sm" />
+      <Spinner size="md" />
+      <Spinner size="lg" />
     </div>
   ),
 };
--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,36 +3,13 @@
 import React from 'react';
 
 interface SpinnerProps {
-  variant?: 'button';
   customClass?: string;
+  size?: 'sm' | 'md' | 'lg';
+  color?: string;
 }
 
 export function Spinner(props: SpinnerProps) {
-  const { variant = 'default', customClass = '' } = props;
-
-  const gradientColorClass =
-    variant === 'button'
-      ? 'bg-gradient-to-tr from-neutral via-primary to-primary'
-      : 'bg-gradient-to-tr from-primary via-neutral to-neutral';
-
-  return (
-    <div
-      data-testid="spinner"
-      className={`animate-spin flex items-center justify-center rounded-full w-6 h-6
-    ${gradientColorClass} ${customClass}`}
-    >
-      <div className="w-5 h-5 rounded-full bg-c-disabled-1" />
-    </div>
-  );
-}
-
-interface ISpinnerProps {
-  customClass?: string;
-  size?: 'sm' | 'md' | 'lg';
-}
-
-export function SpinnerSized(props: ISpinnerProps) {
-  const { customClass = '', size = 'sm' } = props;
+  const { customClass = '', size = 'sm', color = 'neutral' } = props;
 
   const isSmall = size === 'sm';
   const isMedium = size === 'md';
@@ -44,8 +21,9 @@ export function SpinnerSized(props: ISpinnerProps) {
 
   return (
     <div
+      data-testid="spinner"
       className={`animate-spin ${sizeClass} rounded-full border-2 border-t-transparent
-        border-neutral ${customClass}`}
+        border-${color} ${customClass}`}
     />
   );
 }
