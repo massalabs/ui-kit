74a226201b395d940004a04219e40432a826bafd
Nathan Seva <thykof@protonmail.ch>
2024-04-25 07:52:36 -0500

    getIcon: add MAS and size props

--- src/components/Icons/Svg/FT/tokenIcons.tsx
+++ src/components/Icons/Svg/FT/tokenIcons.tsx
@@ -9,39 +9,42 @@ import { TDaiSvg } from './TDaiSvg';
 import { USDCSvg } from './USDCSvg';
 import { WEthMassaSvg } from './WEthMassaSvg';
 import { WEthSvg } from './WEthSvg';
+import { MassaLogo } from '../Massa';
 
 export function getIcon(
   symbolEVM: string,
   nativeToken = false,
   isMainnet = false,
+  size?: number,
 ): ReactNode {
-  const tokens = getTokenIcons(nativeToken, isMainnet);
+  const tokens = getTokenIcons(nativeToken, isMainnet, size);
   const icons = {
     tDAI: tokens.tDAI,
     WETH: tokens.WETH,
     USDC: tokens.USDC,
     DAI: tokens.tDAI,
+    MAS: <MassaLogo size={size} />,
   };
   return icons[symbolEVM as keyof typeof icons];
 }
 
-function getTokenIcons(nativeToken = false, isMainnet = false) {
+function getTokenIcons(nativeToken = false, isMainnet = false, size?: number) {
   if (!nativeToken) {
     return {
-      tDAI: <TDaiSvg />,
-      WETH: <WEthSvg />,
-      USDC: <USDCSvg />,
+      tDAI: <TDaiSvg size={size} />,
+      WETH: <WEthSvg size={size} />,
+      USDC: <USDCSvg size={size} />,
     };
   } else if (isMainnet) {
     return {
-      tDAI: <TDaiMassaSvg />,
-      WETH: <WEthMassaSvg />,
-      USDC: <USDCMassaSvg />,
+      tDAI: <TDaiMassaSvg size={size} />,
+      WETH: <WEthMassaSvg size={size} />,
+      USDC: <USDCMassaSvg size={size} />,
     };
   }
   return {
-    tDAI: <SepoliaDaiSvg />,
-    WETH: <SepoliaWethSvg />,
-    USDC: <SepoliaUSDCSvg />,
+    tDAI: <SepoliaDaiSvg size={size} />,
+    WETH: <SepoliaWethSvg size={size} />,
+    USDC: <SepoliaUSDCSvg size={size} />,
   };
 }
