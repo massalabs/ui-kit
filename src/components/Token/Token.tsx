abdc01816445edace4093fef57b0be80bb2ae579
Nathan Seva <thykof@protonmail.ch>
2024-05-06 16:34:13 -0500

    add dollar value in Token

--- src/components/Token/Token.tsx
+++ src/components/Token/Token.tsx
@@ -5,7 +5,7 @@ import { ComponentPropsWithoutRef } from 'react';
 import { FiTrash2 } from 'react-icons/fi';
 import { Button } from '../Button';
 import { Tooltip } from '../Tooltip';
-import { formatFTAmount } from '../../util/parseAmount';
+import { formatFTAmount, parseAmount } from '../../util/parseAmount';
 
 export interface TokenProps extends ComponentPropsWithoutRef<'div'> {
   logo?: React.ReactNode;
@@ -13,6 +13,7 @@ export interface TokenProps extends ComponentPropsWithoutRef<'div'> {
   symbol: string;
   decimals: number;
   balance: string;
+  dollarValue?: string;
   customClass?: string;
   disable?: boolean;
   onDelete?: () => void;
@@ -25,18 +26,35 @@ export function Token(props: TokenProps) {
     symbol,
     decimals,
     balance,
+    dollarValue,
     customClass = '',
     disable,
     onDelete,
     ...rest
   } = props;
 
-  const bigintBalance = BigInt(balance);
+  let formattedBalance: string | undefined;
+  let rawBalance: string | undefined;
+  let bigintBalance = BigInt(0);
+  if (balance !== '') {
+    bigintBalance = BigInt(balance);
+    const { amountFormattedPreview, amountFormattedFull } = formatFTAmount(
+      bigintBalance,
+      decimals,
+    );
+    rawBalance = amountFormattedFull;
+    formattedBalance = amountFormattedPreview;
+  } else {
+    formattedBalance = undefined;
+    rawBalance = undefined;
+  }
 
-  const {
-    amountFormattedPreview: formattedBalance,
-    amountFormattedFull: rawBalance,
-  } = formatFTAmount(bigintBalance, decimals);
+  let dollarValueFormatted = '';
+  if (dollarValue !== undefined && dollarValue !== '') {
+    const dollarValueBigInt = parseAmount(dollarValue, 2);
+    const { amountFormattedPreview } = formatFTAmount(dollarValueBigInt, 2);
+    dollarValueFormatted = amountFormattedPreview;
+  }
 
   return (
     <div
@@ -48,10 +66,23 @@ export function Token(props: TokenProps) {
         {logo}
         <div className="flex flex-col ">
           <p className="mas-menu-active">{`${name} (${symbol})`} </p>
-          <span className="flex items-center gap-2">
-            <p className="mas-menu">{formattedBalance}</p>
-            <Tooltip body={rawBalance} />
-          </span>
+          <div className="flex items-center gap-2">
+            <span className="flex items-center gap-2">
+              {formattedBalance !== undefined ? (
+                <>
+                  <p className="mas-menu">{formattedBalance}</p>
+                  <Tooltip body={rawBalance} />
+                </>
+              ) : null}
+            </span>
+            <span className="flex items-center gap-2">
+              {dollarValueFormatted !== '' ? (
+                <>
+                  <p className="mas-menu">≈ ${dollarValueFormatted}</p>
+                </>
+              ) : null}
+            </span>
+          </div>
         </div>
       </div>
       <div>
