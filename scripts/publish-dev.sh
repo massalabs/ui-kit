a3e8a7300ccb0db584a480003d65f3dada56d20d
Nathan Seva <thykof@protonmail.ch>
2024-05-15 11:57:10 -0500

    change npm publish tag to next

--- scripts/publish-dev.sh
+++ scripts/publish-dev.sh
@@ -3,14 +3,16 @@ set -e
 
 NPM_PACKAGE=$(cat package.json | jq -r '.name')
 
-npm version --preid dev --no-git-tag-version --no-commit-hooks prepatch
+TAG=next # change it back to dev when we are ready to publish to dev
+
+npm version --preid $TAG --no-git-tag-version --no-commit-hooks prepatch
 #Use timestamp as package suffix
 TIME=$(date -u +%Y%m%d%H%M%S)
-sed -i "/version/s/dev.0/dev.$TIME/g" package.json
+sed -i "/version/s/$TAG.0/$TAG.$TIME/g" package.json
 PUBLISH_VERSION=$(cat package.json | jq -r '.version')
 echo publishing $NPM_PACKAGE@$PUBLISH_VERSION
 
 # disable husky
 npm pkg delete scripts.prepare
 
-npm publish --access public --tag dev
+npm publish --access public --tag $TAG
