cd13ce2c5ffbe38f4b403ce08f137b491361b616
Nathan Seva <thykof@protonmail.ch>
2024-05-16 14:58:43 -0500

    DollarValue: p html element

--- src/components/DollarValue/DollarValue.tsx
+++ src/components/DollarValue/DollarValue.tsx
@@ -17,7 +17,7 @@ export function DollarValue(props: DollarValueProps) {
 
   if (dollarValueError) {
     return (
-      <p
+      <div
         data-testid="dollar-value"
         className={`mas-menu ${customClass}`}
         {...rest}
@@ -25,7 +25,7 @@ export function DollarValue(props: DollarValueProps) {
         <Tooltip body={dollarValueError}>
           <FiAlertTriangle className="text-s-warning" />
         </Tooltip>
-      </p>
+      </div>
     );
   }
 
