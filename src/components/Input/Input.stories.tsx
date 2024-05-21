9ee6007d3256bd90f8d026adc67884caa990a88a
Pivi Lartisant <phoebelartisant@proton.me>
2024-05-21 16:56:26 +0200

    add right text input option (#442)
    * add right text input option
    
    * add react node

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -8,53 +8,58 @@ export const _RegularInput = {
     <>
       <h3>Controlled components</h3>
       <Input placeholder={'with no content'} value="" />
-      <br />
+      <br /> <br />
       <Input placeholder={'something'} value="with content" />
-      <br />
+      <br /> <br />
       <Input
         placeholder={'something'}
         value=""
         error="that's an error without content"
       />
-      <br />
+      <br /> <br />
       <Input
         placeholder={'something'}
         value="with content"
         error="that's an error with content"
       />
-      <br />
+      <br /> <br />
       <Input
         placeholder={'something'}
         value=""
         warning="that's a warning without content"
       />
-      <br />
+      <br /> <br />
       <Input
         placeholder={'something'}
         value="123"
         warning="that's a warning with content"
       />
-      <br />
-
+      <br /> <br />
       <h3>Uncontrolled components</h3>
       <Input placeholder={'something'} defaultValue="" disable={true} />
-      <br />
+      <br /> <br />
       <Input
         placeholder={'something'}
         defaultValue=""
         error="that's an error with|without content"
       />
-      <br />
+      <br /> <br />
       <Input
         placeholder={'something'}
         defaultValue=""
         warning="that's a warning with|without content"
       />
-
+      <br /> <br />
       <Input
         placeholder={'something'}
         defaultValue=""
         success="that's a success with|without content"
+      />
+      <br /> <br />
+      <Input
+        placeholder={'something with right text'}
+        defaultValue=""
+        rightText={'👍'}
       />
     </>
   ),
