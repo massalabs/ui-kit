5b9d08467e0b9de602efebf219933ab3e87fbba8
Pivi Lartisant <phoebelartisant@proton.me>
2024-05-23 14:31:06 +0200

    add handle percent (#446)

---
+++ src/lib/util/handlePercent.ts
@@ -0,0 +1,25 @@
+import { massaToken } from '../massa-react/const';
+import { formatAmount } from './parseAmount';
+
+export function handlePercent(
+  amount = 0n,
+  percent: bigint,
+  fees: bigint,
+  balance: bigint,
+  decimals: number,
+  symbol: string,
+): string {
+  let newAmount = (amount * percent) / 100n;
+
+  if (symbol === massaToken) {
+    if (newAmount > balance - fees) {
+      if (balance - fees < 0) {
+        newAmount = 0n;
+      } else {
+        newAmount = balance - fees;
+      }
+    }
+  }
+
+  return formatAmount(newAmount.toString(), decimals).amountFormattedFull;
+}
