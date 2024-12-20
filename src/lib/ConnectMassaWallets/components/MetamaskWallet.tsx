227526518a5538332a265527a5ec6415945f5b2e
BenRey <44082144+Ben-Rey@users.noreply.github.com>
2024-12-20 16:09:25 +0100

    update wallet-provider version in package-lock.json (#504)
    * update wallet-provider version in package-lock.json
    
    * refactor: remove network switching functionality from MetamaskWallet component

--- src/lib/ConnectMassaWallets/components/MetamaskWallet.tsx
+++ src/lib/ConnectMassaWallets/components/MetamaskWallet.tsx
@@ -8,21 +8,9 @@ import { WalletError } from './WalletError';
 import Intl from '../i18n';
 import { useAccountStore } from '../store';
 import { METAMASK_INSTALL } from '../../massa-react/const';
-import { Button } from '../../../components';
-import { CHAIN_ID, Network } from '@massalabs/massa-web3';
 
 export default function MetamaskWallet() {
-  const { connectedAccount, currentWallet, network, isFetching } =
-    useAccountStore();
-
-  function handleSwitchNetwork(network: Network): void {
-    if (!currentWallet) return;
-    if (network.chainId === CHAIN_ID.Mainnet) {
-      currentWallet.setRpcUrl('https://buildnet.massa.net/api/v2');
-    } else {
-      currentWallet.setRpcUrl('https://mainnet.massa.net/api/v2');
-    }
-  }
+  const { connectedAccount, isFetching } = useAccountStore();
 
   if (!connectedAccount && !isFetching) {
     return (
@@ -40,24 +28,6 @@ export default function MetamaskWallet() {
     <div className="flex flex-col gap-4 mas-body">
       <ConnectedAccount />
       <MASBalance />
-      {network && currentWallet && (
-        <div className="flex gap-4">
-          <SwitchNetwork
-            networkName={network.name === 'mainnet' ? 'buildnet' : 'mainnet'}
-            onClick={() => handleSwitchNetwork(network)}
-          />
-        </div>
-      )}
     </div>
   );
-}
-
-function SwitchNetwork({
-  networkName,
-  onClick,
-}: {
-  networkName: string;
-  onClick: () => void;
-}) {
-  return <Button onClick={onClick}>Switch To {networkName}</Button>;
 }
