467847562400ceea880f21f4ad5bd319c7d4baad
BenRey <br@massa.net>
2025-01-20 16:02:04 +0100

    Refactor MASBalance component to use balance from account store and streamline balance fetching logic

--- src/lib/ConnectMassaWallets/store/accountStore.ts
+++ src/lib/ConnectMassaWallets/store/accountStore.ts
@@ -4,6 +4,7 @@ import { Wallet, WalletName } from '@massalabs/wallet-provider';
 
 export interface AccountStoreState {
   connectedAccount?: Provider;
+  balance?: bigint;
   accounts?: Provider[];
   currentWallet?: Wallet;
   wallets: Wallet[];
@@ -20,12 +21,15 @@ export interface AccountStoreState {
   setWallets: (wallets: Wallet[]) => void;
   setConnectedAccount: (account?: Provider) => void;
   setCurrentNetwork: (network: Network) => void;
+  fetchBalance: (provider: Provider, final: boolean) => Promise<void>;
+  refreshBalance: (final: boolean) => void;
 }
 
 export const useAccountStore = create<AccountStoreState>((set, get) => ({
   accounts: undefined,
   network: undefined,
   connectedAccount: undefined,
+  balance: undefined,
   accountObserver: undefined,
   networkObserver: undefined,
   currentWallet: undefined,
@@ -78,7 +82,7 @@ export const useAccountStore = create<AccountStoreState>((set, get) => ({
   setAccounts: async (wallet: Wallet, account?: Provider) => {
     const accounts = await wallet.accounts();
     set({ accounts });
-    set({ connectedAccount: account || accounts[0] });
+    get().setConnectedAccount(account || accounts[0]);
   },
 
   setWallets: (wallets: Wallet[]) => {
@@ -88,13 +92,26 @@ export const useAccountStore = create<AccountStoreState>((set, get) => ({
     }
   },
 
-  setConnectedAccount: (connectedAccount?: Provider) => {
+  setConnectedAccount: async (connectedAccount?: Provider) => {
     set({ connectedAccount });
+    if (!connectedAccount) return;
+    get().fetchBalance(connectedAccount, false);
   },
 
   setCurrentNetwork: (network: Network) => {
     if (network === get().network) return;
     set({ network });
+  },
+
+  fetchBalance: async (provider: Provider) => {
+    const balance = await provider.balance(false);
+    set({ balance });
+  },
+
+  refreshBalance: async (final: boolean) => {
+    const { connectedAccount } = get();
+    if (!connectedAccount) return;
+    get().fetchBalance(connectedAccount, final);
   },
 }));
 
