73d55fd729e25d11fffed7e3f80bd317ca0ae48c
Andrei Fleiser <fleandrei@yahoo.com>
2025-04-08 18:31:03 +0200

    replace hash by version annotation

--- src/lib/massa-react/hooks/useDisclaimer.tsx
+++ src/lib/massa-react/hooks/useDisclaimer.tsx
@@ -4,55 +4,53 @@ interface DisclaimerProps {
   docs: { title: string; link: string }[];
 }
 
-interface hashFile {
-  hash: string;
+interface docVersion {
+  version: number;
   link: string;
 }
 
 const LocalStorageDisclaimerPrefix = 'DEWEB_UPLOADER_DISCLAIMER_';
 
+/* Links passed to useDisclaimer should end with "_V{version number}.{extension}" */
 export function useDisclaimer({ docs }: DisclaimerProps) {
   const [isValid, setIsValid] = useState(true);
   const [error, setError] = useState<Error | null>(null);
-  const [newOrUpdatedFiles, setNewOrUpdatedFile] = useState<hashFile[]>([]);
+  const [newOrUpdatedFiles, setNewOrUpdatedFile] = useState<docVersion[]>([]);
 
   /* For each doc, check whether it has been accepted or not by the user.
-    When a doc has been accepted, it's hash is stored in the localstorage (the key is it's link).
-    So if a doc is updated, it's hash would not match the one stored in the localstorage and the 
-    user would be invited again to accept (updated) docs.
-    If the hash of the doc match the one in the localstorage, it means that the user has already accepted them so
-    no need to display the disclaimer */
+        When a doc has been accepted, it's hash is stored in the localstorage (the key is it's link).
+        So if a doc is updated, it's hash would not match the one stored in the localstorage and the 
+        user would be invited again to accept (updated) docs.
+        If the hash of the doc match the one in the localstorage, it means that the user has already accepted them so
+        no need to display the disclaimer */
   useEffect(() => {
     async function checkLegalDoc() {
-      const newOrUpdated: hashFile[] = [];
+      const newOrUpdated: docVersion[] = [];
       for (const doc of docs) {
-        try {
-          const response = await fetch(doc.link);
-          const content = await response.text();
-
-          // hash of the document content
-          const hash = await crypto.subtle.digest(
-            'SHA-256',
-            new TextEncoder().encode(content),
+        // extract version number from the link
+        const versionMatch = doc.link.match(/_V(\d+)\.[a-zA-Z]{1,5}$/);
+        if (!versionMatch) {
+          setError(
+            new Error(
+              `Invalid document link format: ${doc.link}. Should have a version number`,
+            ),
           );
-          const hashHex = Array.from(new Uint8Array(hash))
-            .map((b) => b.toString(16).padStart(2, '0'))
-            .join('');
+          return;
+        }
+        const version = versionMatch[1];
 
-          // check if the hash is already stored in localstorage
-          const storedHash = localStorage.getItem(
-            LocalStorageDisclaimerPrefix + doc.link,
-          );
+        // check if the version is already stored in localstorage
+        const storedVersion = localStorage.getItem(
+          LocalStorageDisclaimerPrefix + doc.link,
+        );
 
-          // if the hash is not stored or it is different from the current hash, the user has to accept the doc again
-          if (!storedHash || storedHash !== hashHex) {
-            newOrUpdated.push({
-              link: doc.link,
-              hash: hashHex,
-            });
-          }
-        } catch (error) {
-          setError(error as Error);
+        /* if the version is not stored or it is different from the current version,
+                the user has to accept the doc again */
+        if (!storedVersion || storedVersion !== version) {
+          newOrUpdated.push({
+            link: doc.link,
+            version: parseInt(version, 10), // convert version to number
+          });
         }
       }
       setNewOrUpdatedFile(newOrUpdated);
@@ -62,8 +60,8 @@ export function useDisclaimer({ docs }: DisclaimerProps) {
     checkLegalDoc();
 
     /* Remove from localstorage all entries whose key begin by LocalStorageDisclaimerPrefix
-    and that are not in the docs list.
-    This is done to remove docs that are no more required */
+        and that are not in the docs list.
+        This is done to remove docs that are no more required */
     return () => {
       const storedKeys = Object.keys(localStorage).filter((key) =>
         key.startsWith(LocalStorageDisclaimerPrefix),
@@ -80,8 +78,11 @@ export function useDisclaimer({ docs }: DisclaimerProps) {
   }, [docs]);
 
   function accept() {
-    newOrUpdatedFiles.forEach(({ link, hash }) => {
-      localStorage.setItem(LocalStorageDisclaimerPrefix + link, hash);
+    newOrUpdatedFiles.forEach(({ link, version }) => {
+      localStorage.setItem(
+        LocalStorageDisclaimerPrefix + link,
+        version.toString(),
+      );
     });
 
     setIsValid(true);
